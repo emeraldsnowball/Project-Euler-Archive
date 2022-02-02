41d35543dfd6b6cbdfbc9ff290d986901f06960d
mable <0rickymandloi@gmail.com>
2022-02-02 10:01:04 +0000

    problem3

---
+++ problem003.py
@@ -0,0 +1,22 @@
+import math
+
+def largestPrimeFactor(arg1):
+    n = arg1
+
+    maxprime = 1
+
+    if n % 2 == 0:
+      maxprime = 2
+      return maxprime
+    
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+      if n % i == 0:
+         maxprime = i
+         n = n / i
+    
+    if n > 2:
+      maxprime = n
+    
+    return int(maxprime)
+
+print(largestPrimeFactor(600851475143))
